8a4e0148442289af58a3b5a7c9458b6f8098739e
Hà Thạch Thanh Tú <htttu251206@gmail.com>
2025-10-17 19:02:07 +0700

    fix login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/home" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </Router>
     </div>
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import classes from './AuthenticationTitle.module.css';
 
-const Login = () => {
+export default function Login() {
   return (
-    <div>Login</div>
-  )
-}
+    <div className={classes.loginWrapper}>
+      <div className={classes.header}>
+        <h1 className={classes.title}>Welcome back!</h1>
+      </div>
 
-export default Login
+      <div className={classes.formContainer}>
+        <form>
+          <div className={classes.inputGroup}>
+            <label htmlFor="email">Email *</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="you@example.dev"
+              required
+            />
+          </div>
+
+          <div className={classes.inputGroup}>
+            <label htmlFor="password">Password *</label>
+            <div className={classes.passwordWrapper}>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Your password"
+                    required
+                />
+            </div>
+          </div>
+
+          <div className={classes.options}>
+            <div className={classes.rememberMe}>
+              <input className='cursor-pointer' type="checkbox" id="remember" />
+              <label className='cursor-pointer' htmlFor="remember">Remember me</label>
+            </div>
+          </div>
+
+          <button type="submit" className={classes.signInButton}>
+            Sign in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
