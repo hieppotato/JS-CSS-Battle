a488180a9ccec51753ea2d93fc0a2deaef3e3fd5
Hà Thạch Thanh Tú <htttu251206@gmail.com>
2025-10-20 21:40:51 +0700

    add more

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
+import UserDashboard from './pages/Home/UserDashboard';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
           <Route path="/" element={<Home/>}/>
           <Route path="/home" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/dashboard" element={<UserDashboard/>}/>
         </Routes>
       </Router>
     </div>
