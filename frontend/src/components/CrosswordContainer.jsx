45c61b603319df0a3a905bda077fd25cd9b76676
Nguyen Trong Hiep <hieppotato2@hotmail.com>
2025-10-25 00:06:18 +0700

    fix

--- frontend/src/components/CrosswordContainer.jsx
+++ frontend/src/components/CrosswordContainer.jsx
@@ -344,8 +344,6 @@ const CrosswordContainer = ({ puzzleId, userInfo, setScoreFromServer }) => {
     }, 0);
   }
 
-  console.log(countOccurrences(userInfo.hints, "13"));
-
   const handleVerticalSubmit = async () => {
   if (!verticalGuess.trim()) {
     alert('Vui lòng nhập chữ hàng dọc.');
